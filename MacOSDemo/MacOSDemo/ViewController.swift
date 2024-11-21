19aa56abf87796ace77ae3c6b1c5fbc2ae739e3c
tm <>
2024-11-21 16:14:25 +0800

    添加项目

---
+++ MacOSDemo/MacOSDemo/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MacOSDemo
+//
+//  Created by BZ on 2024/11/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
